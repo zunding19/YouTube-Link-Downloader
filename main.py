dd1deac5b97c5654e5b9bde1a8b5b72a6fd9954e
Han Zun Ding <zhan0044@student.monash.edu>
2026-09-01 22:28:54 +0800

    improved error handling so invalid URL wont completely break the app

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
+from fastapi import FastAPI, HTTPException
 import yt_dlp
 
 app = FastAPI()
@@ -10,3 +11,30 @@ class VideoRequest(BaseModel):
 @app.get("/") #when someone visits, run the function
 def home():
     return {"message": "Backend is working"}
+
+@app.post("/download")
+def download_video(request: VideoRequest):
+
+    url = request.url
+
+    options = {
+        "format": "best",
+        "outtmpl": "downloads/%(title)s.%(ext)s"
+    }
+
+    try:
+
+        with yt_dlp.YoutubeDL(options) as ydl:
+            ydl.download([url])
+
+        return {
+            "message": "Download completed"
+        }
+
+    except Exception as error:
+
+        raise HTTPException(
+            status_code=400,
+            detail=str(error)
+        )
+
