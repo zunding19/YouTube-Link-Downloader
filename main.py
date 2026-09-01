3a15a13a976db529f981aa5d34d2b4d3d709246e
Han Zun Ding <zhan0044@student.monash.edu>
2026-09-01 22:20:25 +0800

    created data model for the youtube URL

--- main.py
+++ main.py
@@ -4,3 +4,9 @@ import yt_dlp
 
 app = FastAPI()
 
+class VideoRequest(BaseModel):
+    url: str #expect some data containing url = string
+
+@app.get("/") #when someone visits, run the function
+def home():
+    return {"message": "Backend is working"}
